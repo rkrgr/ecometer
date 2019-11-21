7f5cc9b90e6a5981a8cf0804db0264deeddcc796
rkrgr <r.krgr@tuta.io>
2019-11-21 21:56:57 +0100

    set up starting page

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
