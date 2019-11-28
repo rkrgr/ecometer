61b549ac423e5118efed23150a79e3ca5db40886
rkrgr <r.krgr@tuta.io>
2019-11-28 14:37:24 +0100

    Add handlebars helper function to format date

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const exphbs = require('express-handlebars');
 const app = express();
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ 
+    defaultLayout: 'main',
+    helpers: require('./config/handlebarsHelpers')
+ }));
 app.set('view engine', 'handlebars');
 
 app.use('/', require('./routes/index.js'));
