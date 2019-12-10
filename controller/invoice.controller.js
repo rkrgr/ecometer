bbfc45f1eab06683786a13c0f814ce39284e3351
GeradeMalda <gerardschade@gmail.com>
2019-12-10 19:37:21 +0100

    change invoice.controller.js to accept invoice and invoices

--- controller/invoice.controller.js
+++ controller/invoice.controller.js
@@ -2,6 +2,12 @@ const invoiceService = require("../services/invoice.service")
 
 module.exports = {
     invoice: async (req, res) => {
+        const invoices = await invoiceService.getInvoices(1);
+        res.render('invoice', {
+            invoices
+        });
+    },
+    invoices: async (req, res) => {
         const invoices = await invoiceService.getInvoices(2);
         res.render('invoice', {
             invoices
