794be5b2dc4591c309752440ef7fa583e8834cc1
GeradeMalda <gerardschade@gmail.com>
2019-12-10 16:57:49 +0100

    change invoice.controller to async fuction

--- controller/invoice.controller.js
+++ controller/invoice.controller.js
@@ -1,10 +1,10 @@
 const invoiceService = require("../services/invoice.service")
 
 module.exports = {
-    invoice: (req, res) => {
-        const invoices = invoiceService.getInvoices(2);
+    invoice: async (req, res) => {
+        const invoices = await invoiceService.getInvoices(2);
         res.render('invoice', {
             invoices
-        })
+        });
     }
 }
