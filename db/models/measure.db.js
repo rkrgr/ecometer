1a20ffbb40c0384b31094082562d706a322a9ee7
rkrgr <r.krgr@tuta.io>
2019-12-13 12:36:29 +0100

    Use new database tables for measures

--- db/models/measure.db.js
+++ db/models/measure.db.js
@@ -2,10 +2,12 @@ const moment = require("moment")
 
 const db = require('../connection')
 
+const tableName = 'tbl_massnahme'
+
 module.exports = {
     getMeasure: (id) => {
         return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM measure WHERE id=?', id, (err, rows) => {
+            db.query('SELECT * FROM ' + tableName + ' WHERE massnahme_ID=?', id, (err, rows) => {
                 if (err) {
                     reject(err)
                 } else {
@@ -16,29 +18,38 @@ module.exports = {
     },
     getLatestMeasures: (num) => {
         return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM measure ORDER BY timestamp DESC LIMIT ?', num, (err, rows) => {
+            db.query('SELECT * FROM ' + tableName + ' ORDER BY massnahme_datum DESC LIMIT ?', num, (err, rows) => {
                 if (err) {
                     reject(err)
                 } else {
-                    resolve(rows)
+                    let results = []
+                    rows.forEach((row) => {
+                        results.push({
+                            name: row.massnahme_name,
+                            timestamp: row.massnahme_datum,
+                            companyId: row.fk_mass_unternehmen
+                        })
+                    })
+                    resolve(results)
                 }
             })
         })
     },
-    insertMeasure: (measure) => { 
+    insertMeasure: (measure) => {
         return new Promise((resolve, reject) => {
-            db.query('INSERT INTO measure (name, co2Saving, timestamp, companyId) VALUES (?,?,?,?)', [measure.name, measure.co2Saving, measure.timestamp.format('YYYY-MM-DD'), measure.companyId], (err, result) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(result.insertId)
-                }
-            })
+            db.query('INSERT INTO ' + tableName + ' (massnahme_name, massnahme_co2einsparung, massnahme_datum, fk_mass_unternehmen, fk_mass_einheit, fk_mass_kategorie, massnahme_offentlich) VALUES (?,?,?,?,?,?,?)',
+                [measure.name, measure.co2Saving, measure.timestamp.format('YYYY-MM-DD'), measure.companyId, 1, 1, true], (err, result) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(result.insertId)
+                    }
+                })
         })
     },
     deleteMeasure: (id) => {
         return new Promise((resolve, reject) => {
-            db.query('DELETE FROM measure WHERE id=?', id, (err, result) => {
+            db.query('DELETE FROM ' + tableName + ' WHERE massnahme_ID=?', id, (err, result) => {
                 if (err) {
                     reject(err)
                 } else {
