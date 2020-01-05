03d89c5aa6cd16c852721e787dc8e13b2e5485ed
GeradeMalda <gerardschade@gmail.com>
2020-01-05 17:32:22 +0100

    update invoice.controler to fit with database

--- controller/invoice.controller.js
+++ controller/invoice.controller.js
@@ -17,7 +17,19 @@ module.exports = {
         res.render('invoiceinsert')
     },
     invoice_insert: async (req, res) => {
-        const insertInvoice = await invoiceService.insertInvoice(7); //attention id alleady exists
+        var invoice = {}
+        invoice.fk_rechn_kategorie = req.body.fk_rechn_kategorie;
+        invoice.rechnung_verbrauchswert = req.body.rechnung_verbrauchswert;
+        invoice.fk_rechn_einheit = req.body.fk_rechn_einheit;
+        invoice.rechnung_emissionsfaktor = req.body.rechnung_emissionsfaktor;
+        invoice.rechnungsdaten_startdatum = req.body.rechnungsdaten_startdatum;
+        invoice.rechnung_enddatum = req.body.rechnung_enddatum;
+        console.log(invoice);   
+        //var body = JSON.stringify(req.body)     
+        //console.log(req.body.fk_rechn_einheit)
+        //console.log(body)
+        //console.log(req.body.fk_rechn_einheit)
+        const insertInvoice = await invoiceService.insertInvoice(invoice);
         res.render('insertinvoice', {
             insertInvoice
         });
