378d8ed81789fb2be5148f22dd76fd563f48373a
Anastasiia-02 <anastasiiahorodniuk@gmail.com>
2020-01-06 14:38:58 +0100

    measures view

--- controller/createMeasures.controller.js
+++ controller/createMeasures.controller.js
@@ -2,5 +2,9 @@ module.exports = {
     index: async (req, res) => {
             
             res.render('createMeasures');
+
+            myMassnahme :request.body.myMassnamhe, 
+            myEinheit : request.body.myEinheit,       
+            nachname : request.body.nachname,
     }
 }
--- routes/index.router.js
+++ routes/index.router.js
@@ -4,8 +4,8 @@ const homeController = require('../controller/home.controller')
 
 router.use('/login', require('./login.router'))
 router.use('/invoice', require('../routes/invoice.js'))
-router.use('/allMeasures', require('../routes/allMeasures.router'))
-router.use('/createMeasures', require('../routes/createMeasures.router'))
+router.use('/measures', require('../routes/allMeasures.router'))
+
 
 router.get('/', homeController.index)
 
