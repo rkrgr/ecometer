3c468a7e2b7d78512fe133b8e20941548ea483f9
Anastasiia-02 <anastasiiahorodniuk@gmail.com>
2020-01-06 12:17:28 +0100

    add measures form

--- routes/createMeasures.router.js
+++ routes/createMeasures.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const createMeasuresController = require('../controller/allMeasures.controller');
+const createMeasuresController = require('../controller/createMeasures.controller');
 
 router.get('/', createMeasuresController.index);
 
