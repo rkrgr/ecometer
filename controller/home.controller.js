4e032caa188f28cca7f3a302e7c57d56f4a26e5d
rkrgr <r.krgr@tuta.io>
2020-01-07 15:40:10 +0100

    Add history line chart to home page

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,13 +1,16 @@
+const invoiceService = require("../services/invoice.service");
 const measureService = require("../services/measure.service");
 
 module.exports = {
     index: async (req, res) => {
-            const latestMeasures = await measureService.getLatestMeasures(3);
-            const bestMeasures = await measureService.getBestMeasures(3);
-            res.render('index', {
-                user: req.user,
-                latestMeasures,
-                bestMeasures
-            });
+        const historyMap = await invoiceService.getHistoryMap();
+        const latestMeasures = await measureService.getLatestMeasures(3);
+        const bestMeasures = await measureService.getBestMeasures(3);
+        res.render('index', {
+            user: req.user,
+            historyMap: JSON.stringify(Array.from(historyMap.entries())),
+            latestMeasures,
+            bestMeasures
+        });
     }
 }
