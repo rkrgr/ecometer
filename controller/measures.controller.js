097df4b70e3579e3b75d2f8671615148bb1f2ae2
Anastasiia-02 <anastasiiahorodniuk@gmail.com>
2020-01-08 16:56:58 +0100

    all measures mit form doesnt works

--- controller/measures.controller.js
+++ controller/measures.controller.js
@@ -1,4 +1,7 @@
 const measuresService = require("../services/measures.service");
+//const format = require("moment-format");
+const moment = require("moment");
+
 
 module.exports = {
     allMeasures: async (req, res) => {
@@ -21,7 +24,8 @@ module.exports = {
         measure.massnahme_absoluteeinsaprung=req.body.massnahme_absoluteeinsaprung;
         measure.tbl_kategorie_einheit=req.body.fk_mass_einheit;
         measure.massnahme_co2einsparung=req.body.massnahme_co2einsparung;
-        measure.tbl_kategorie_einheit=req.body.fk_mass_einheit
+        measure.tbl_kategorie_einheit=req.body.fk_mass_einheit;
+        console.log(measure);
         const insertMeasure = await measuresService.insertMeasure(measure);
         //res.render('insertinvoice', {
         //    insertInvoice
