0534aaaa33a50d14a5a633c75be1c78f88647bed
s0558274 <s0558274@htw-berlin.de>
2020-01-11 16:50:23 +0100

    connected to db, but insert not possible

--- controller/measures.controller.js
+++ controller/measures.controller.js
@@ -4,39 +4,42 @@ const moment = require("moment");
 
 
 module.exports = {
-    allMeasures: async (req, res) => {
-            const allMeasures = await measuresService.getAllMeasures(3);
-            res.render('allMeasures', {
-                allMeasures
-            });
-    },
+        allMeasures: async (req, res) => {
+                const allMeasures = await measuresService.getAllMeasures(3);
+                res.render('allMeasures', {
+                    allMeasures
+                });
+        },
     
         createMeasures: async (req, res) => {
                 
                 res.render('createMeasures');
-    
                
         },
+
         measure_insert:async (req, res)=>{
             var measure = {}
+            
+            //fk_mass_einheit, fk_mass_kategorie, fk_mass_unternehmen  
+            measure.massnahme_name=req.body.massnahme_name;
+            //measure.massnahme_datum.format('YYYY-MM-DD')="02.11.2019";
+            measure.massnahme_absoluteeinsaprung=parseFloat(req.body.massnahme_absoluteeinsaprung);
+            measure.massnahme_co2einsparung=parseFloat(req.body.massnahme_co2einsparung);
+            measure.tbl_kategorie_einheit=1;
+            measure.tbl_kategorie_einheit=1;
+            measure.fk_mass_unternehmen=1;
+
             console.log(measure);
+
             const insertMeasure = await measuresService.insertMeasure(measure);
 
-        measure.massnahme_name=req.body.massnahme_name;
-        measure.massnahme_absoluteeinsaprung=req.body.massnahme_absoluteeinsaprung;
-        //measure.tbl_kategorie_einheit=req.body.fk_mass_einheit;
-        measure.massnahme_co2einsparung=req.body.massnahme_co2einsparung;
-        measure.tbl_kategorie_einheit=req.body.fk_mass_einheit;
-        measure.massnahme_datum.format('YYYY-MM-DD')="02.11.2019";
-        measure.fk_mass_unternehmen="1";
-
-        
-        //res.render('insertinvoice', {
-        //    insertInvoice
-        //});
-        //res.redirect(307, '/test');
-        //res.send({redirect: '/invoice/invoiceinsert'});
-        res.redirect('../measure/createMeasure')        
+            //res.render('insertinvoice', {
+            //    insertInvoice
+            //});
+            //res.redirect(307, '/test');
+            //res.send({redirect: '/invoice/invoiceinsert'});
+            console.log(measure);
+            res.redirect('../measure')        
 
         }
         
